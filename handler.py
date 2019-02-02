5ad0af0264d8ad2a77ad9d70472310e343e5cf2d
Manuel Martinez <mm16356@bristol.ac.uk>
2019-02-02 23:08:22 +0000

    Preliminary sum handler

--- handler.py
+++ handler.py
@@ -20,41 +20,41 @@ CALCULATOR_TOPICS = ['hermes/intent/ComputeSum']
 # Subscribe to msg stream
 def on_connect(client, userdata, flags, rc):
     for topic in WEATHER_TOPICS:
-	mqtt_client.subscribe(topic)
-	
+    mqtt_client.subscribe(topic)
+    
 def on_connect(client, userdata, msg):
-	print(msg.topic)
-	
-	if msg.topic not in CALCULATOR_TOPICS:
-		return
-		
-	slots = parse_slots(msg)
-	sum = addTwo(slots)
-	
-	if msg.topic == 'hermes/intent/ComputeSum':
-		response = ("The sum is {0}.".format(sum["sum"]))
-		
-	session_id = parse_session_id(msg)
-	say(session_id, response)
-	
+    print(msg.topic)
+    
+    if msg.topic not in CALCULATOR_TOPICS:
+        return
+        
+    slots = parse_slots(msg)
+    sum = addTwo(slots)
+    
+    if msg.topic == 'hermes/intent/ComputeSum':
+        response = ("The sum is {0}.".format(sum["sum"]))
+        
+    session_id = parse_session_id(msg)
+    say(session_id, response)
+    
 def addTwo(slots):
-	'''
-	parse numeber slots and add
-	'''
-	number1 = slots.get("FirstTerm", None)
-	number2 = slots.get("SecondTerm", None)
-	sum = number1 + number2
-	return {"sum": sum}
-	
-	
+    '''
+    parse numeber slots and add
+    '''
+    number1 = slots.get("FirstTerm", None)
+    number2 = slots.get("SecondTerm", None)
+    sum = number1 + number2
+    return {"sum": sum}
+    
+    
 def say(session_id, text):
-	'''
-	Prints the output text (if debig) to the console and TTS engine
-	'''
-	if PRINT_DEBUG_TO_TERMINAL:
-		print(text)
-	mqtt_client.publish('hermes/dialogueManager/endSession', json.dumps({'text': text, "sessionId" : session_id}))
-	
+    '''
+    Prints the output text (if debig) to the console and TTS engine
+    '''
+    if PRINT_DEBUG_TO_TERMINAL:
+        print(text)
+    mqtt_client.publish('hermes/dialogueManager/endSession', json.dumps({'text': text, "sessionId" : session_id}))
+    
 def parse_session_id(msg): 
     '''
     Extract the session id from the message
@@ -67,10 +67,11 @@ def parse_slots(msg):
     We extract the slots as a dict
     '''
     data = json.loads(msg.payload)
-	return {slot['slotName']: slot['rawValue'] for slot in data['slots']}	
-	
+    return {slot['slotName']: slot['rawValue'] for slot in data['slots']}    
+    
+    
 if __name__ == '__main__':
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
     mqtt_client.connect(HOST, PORT)
-	mqtt_client.loop_forever()
+    mqtt_client.loop_forever()
