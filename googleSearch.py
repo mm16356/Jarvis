5b40eec5b4f7058f6e6efcde5a94bb7d4a021022
Manuel Martinez <mm16356@bristol.ac.uk>
2019-02-03 04:12:37 +0000

    Preliminary sum handler

--- googleSearch.py
+++ googleSearch.py
@@ -1,10 +1,13 @@
+import webbrowser as wb
+
 try: 
 	from googlesearch import search 
 except ImportError: 
 	print("No module named 'google' found") 
 
 # to search 
-query = "Geeksforgeeks"
+query = "filetype:pdf"+ "bc547"
 
-for j in search(query, tld="co.in", num=10, stop=1, pause=2): 
-	print(j) 
+url =search(query, tld="co.in", num=1, stop=1, pause=2)
+
+wb.open_new_tab(url)
