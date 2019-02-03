8ade60d59e74ea28c940a1c964c3397273123359
Manuel Martinez <mm16356@bristol.ac.uk>
2019-02-03 06:05:48 +0000

    Preliminary sum handler

--- handler2.py
+++ handler2.py
@@ -12,6 +12,13 @@ import requests
 from gpiozero import LED
 from time import sleep
 
+import webbrowser as wb
+
+try: 
+    from googlesearch import search 
+except ImportError: 
+    print("No module named 'google' found") 
+
 led = LED(17)
 
 PRINT_DEBUG_TO_TERMINAL = True
@@ -23,7 +30,10 @@ mqtt_client = mqtt.Client()
 HOST = "localhost"
 PORT = 1883
 
-CALCULATOR_TOPICS = ['hermes/intent/mm16356:ComputeSum','hermes/intent/mm16356:TurnOn','hermes/intent/mm16356:TurnOff']
+_INTENT_SEARCH_DATASHEET = 'hermes/intent/mm16356:demoSearchDatasheet'
+_USER_RANDOM_ANSWER = 'hermes/intent/mm16356:userRandomAnswer'
+
+CALCULATOR_TOPICS = [_INTENT_SEARCH_DATASHEET,_USER_RANDOM_ANSWER]
 
 # Subscribe to msg stream
 def onConnect(client, userdata, flags, rc):
@@ -35,63 +45,112 @@ def onConnect(client, userdata, flags, rc):
 
 def onMessage(client, userdata, msg):
     print("got to on message")
-
+    data = json.loads(message.payload)
+    sessionId = data['sessionId']
+    if message.topic == _INTENT_SEARCH_DATASHEET:
+        ask(text='For which component?', customData=json.dumps({
+        'wasIntent': _INTENT_SEARCH_DATASHEET
+        }))
+    elif message.topic == 'userRandomAnswer'
+        customData = parseCustomData(message)
+        print(customData['userInput'])
+        query = "filetype:pdf datasheet " + customData['userInput']
+        for j in search(query, tld="co.uk", num=1, stop=1, pause=2): 
+            print(j) 
+            wb.open_new_tab(j)
+#    session_id = parse_session_id(msg)
+#    say(response)
     
-    if msg.topic not in CALCULATOR_TOPICS:
+
+def onSessionStarted(client, data, msg):
+    sessionId = parseSessionId(msg)
+    sessions[sessionId] = msg
+
+
+def onSessionEnded(client, data, msg):
+    sessionId = parseSessionId(msg)
+    if sessionId in sessions:
+        del sessions[sessionId]
+
+def onIntentNotRecognized(client, data, msg):
+    payload = json.loads(msg.payload)
+    sessionId = parseSessionId(msg)
+
+    wasMessage = sessions[sessionId]
+    customData = parseCustomData(wasMessage)
+
+    # This is not a continued session as there's no custom data
+    if customData is None:
         return
-        
-    
-    #say(session_id, "Hello")
-    
-    if msg.topic == 'hermes/intent/mm16356:ComputeSum':
-        slots = parse_slots(msg)
-        sum = addTwo(slots)
-        response = ("The sum is {0}.".format(sum["sum"]))
-    if msg.topic == 'hermes/intent/mm16356:TurnOn':
-        led.on()
-        response = ("Machine is on")
-    if msg.topic == 'hermes/intent/mm16356:TurnOff':
-        led.off()
-        response = ("Machine is off")
-    session_id = parse_session_id(msg)
-    say(response)
+
+    siteId = parseSiteId(wasMessage)
+
+    customData['userInput'] = payload['input']
+    payload['customData'] = customData
+
+    payload['siteId'] = siteId
+    wasMessage.payload = json.dumps(payload)
+    wasMessage.topic = 'userRandomAnswer'
+
+    onMessage(None, None, wasMessage)
+
+def endTalk(sessionId, text):
+    mqttClient.publish('hermes/dialogueManager/endSession', json.dumps({
+        'sessionId': sessionId,
+        'text': text
+    }))
 
 
-
-def say(session_id, text):
-    '''
-    Prints the output text (if debig) to the console and TTS engine
-    '''
-    if PRINT_DEBUG_TO_TERMINAL:
-        print(text)
-    mqtt_client.publish('hermes/dialogueManager/endSession', json.dumps({'text': text, "sessionId" : session_id}))
-
-
-def parse_session_id(msg): 
-    '''
-    Extract the session id from the message
-    '''
-    data = json.loads(msg.payload)
-    return data['sessionId']
-
-def parse_slots(msg):
-    '''
-    We extract the slots as a dict
-    '''
-    data = json.loads(msg.payload)
-    return {slot['slotName']: slot['value']['value'] for slot in data['slots']}    
-    
-    
 def say(text):
-    mqtt_client.publish('hermes/dialogueManager/startSession', json.dumps({'init': {'type': 'notification','text': text}}))
-    
+    mqttClient.publish('hermes/dialogueManager/startSession', json.dumps({
+        'init': {
+            'type': 'notification',
+            'text': text
+        }
+    }))
+
+
+def ask(text, client='default', intentFilter=None, customData=''):
+    mqttClient.publish('hermes/dialogueManager/startSession', json.dumps({
+        'siteId': client,
+        'customData': customData,
+        'init': {
+            'type': 'action',
+            'text': text,
+            'canBeEnqueued': True
+        }
+}))
+
+def parseSessionId(message):
+    data = json.loads(message.payload)
+    if 'sessionId' in data:
+        return data['sessionId']
+    else:
+        return False
+
+def parseCustomData(message):
+    data = json.loads(message.payload)
+    if 'customData' in data and data['customData'] is not None:
+        return json.loads(data['customData'])
+    else:
+        return None
+
+
+def parseSiteId(message):
+    data = json.loads(message.payload)
+    if 'siteId' in data:
+        return data['siteId']
+    else:
+        return 'default'
 
 if __name__ == '__main__':
     mqtt_client.on_connect = onConnect
     mqtt_client.on_message = onMessage
+    mqttClient.message_callback_add("hermes/nlu/intentNotRecognized", onIntentNotRecognized)
+    mqttClient.message_callback_add('hermes/dialogueManager/sessionEnded', onSessionEnded)
+    mqttClient.message_callback_add('hermes/dialogueManager/sessionStarted', onSessionStarted)
     mqtt_client.connect(HOST, PORT)
     print("Demo loaded")
     say("This is Jarvis. What do you want?")
     print("Sound played")
-    mqtt_client.connect(HOST, PORT)
     mqtt_client.loop_forever()
